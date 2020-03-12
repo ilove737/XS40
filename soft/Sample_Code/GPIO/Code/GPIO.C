12e2d11d0df836bdaa1722a5ee71be89da5211c7
lxs <lxs>
2020-03-12 16:49:10 +0800

    添加创建帧函数

--- soft/Sample_Code/GPIO/Code/GPIO.C
+++ soft/Sample_Code/GPIO/Code/GPIO.C
@@ -3,6 +3,7 @@
 #include "Function_define.h"
 #include "Common.h"
 #include "Delay.h"
+#include <string.h>
 
 #define row1 P00
 #define row2 P01
@@ -20,11 +21,28 @@
 #define col8 P17
 
 unsigned int i;
-unsigned char xdata beforeAllKey[5]; // 40个位，保存所有40个建的状态
-unsigned char xdata allKey[5];		 // 40个位，保存所有40个建的状态
+unsigned char xdata beforeAllKey[5]; // 40个位，保存上一次所有40个建的状态
+unsigned char xdata allKey[5];		 // 40个位，保存当前所有40个建的状态
+
+unsigned char xdata HIDFrames[8];
 
 UINT8 tttt = 0;
 UINT16 TH1_INIT = 60000;
+
+void makeHIDFrames(void)
+{
+	if (allKey[0] = 0 && allKey[1] = 0 && allKey[2] = 0 && allKey[3] = 0 && allKey[4] = 0)
+	{
+		for (i = 0; i < 8; i++)
+		{
+			Send_Data_To_UART0(0x00);
+		}
+	}
+	else
+	{
+		Send_Data_To_UART0(0x01);
+	}
+}
 
 // 系统时钟      16000 / 16000000 = 0.001s
 // 系统时钟1/12  1333  / 1333333  = 0.001s
@@ -40,36 +58,30 @@ void Timer1_ISR(void) interrupt 3 //interrupt address is 0x001B
 		tttt = 0;
 		if (memcmp(beforeAllKey, allKey, 5) != 0)
 		{
-			for (i = 0; i < 5; i++)
-			{
-				Send_Data_To_UART0(allKey[i]);
-			}
+			memcpy(beforeAllKey, allKey, 5);
+
+			makeHIDFrames();
+			// Send_Data_To_UART0(allKey[i]);
 		}
 	}
-	else
-	{
-		if (tttt == 0)
-			P00 = 0;
-		if (tttt == 1)
-			P01 = 0;
-		if (tttt == 2)
-			P02 = 0;
-		if (tttt == 3)
-			P03 = 0;
-		if (tttt == 4)
-			P04 = 0;
+}
+else
+{
+	if (tttt == 0)
+		P00 = 0;
+	if (tttt == 1)
+		P01 = 0;
+	if (tttt == 2)
+		P02 = 0;
+	if (tttt == 3)
+		P03 = 0;
+	if (tttt == 4)
+		P04 = 0;
 
-		allKey[tttt] = ~P1;
+	allKey[tttt] = ~P1;
 
-		// Send_Data_To_UART0(tttt);
-		// Send_Data_To_UART0(P0);
-
-		// // 判断col1到col8的状态
-		// // Send_Data_To_UART0(~P1);
-		// Send_Data_To_UART0(allKey[tttt]);
-
-		tttt++;
-	}
+	tttt++;
+}
 }
 
 void main(void)
@@ -93,7 +105,6 @@ void main(void)
 	P03_PushPull_Mode;
 	P04_PushPull_Mode;
 
-
 	TIMER1_MODE1_ENABLE;
 	// set_T1M;
 	TH1 = (65536 - TH1_INIT) / 256;
@@ -109,7 +120,5 @@ void main(void)
 	while (1)
 	{
 		set_WDCLR; // 喂狗
-
-		// P04 = ~P04;
 	}
 }
