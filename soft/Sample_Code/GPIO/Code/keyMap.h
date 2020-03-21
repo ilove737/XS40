b433f8b69511806ffd4ed214dfd284dcc04f80c4
black <rj45usb@163.com>
2020-03-21 23:39:16 +0800

    硬件验证结束，单片机少一根GND线

--- soft/Sample_Code/GPIO/Code/keyMap.h
+++ soft/Sample_Code/GPIO/Code/keyMap.h
@@ -2,19 +2,19 @@
 #include "keyScanCode.h"
 
 // 这个就是键盘布局
-// unsigned char keyMap[40] ={
-//     KEY_ESC,        KEY_1,      KEY_2,  KEY_3,  KEY_4,  KEY_5,  KEY_6,  KEY_7,
-//     KEY_TAB,        KEY_Q,      KEY_W,  KEY_E,  KEY_R,  KEY_T,  KEY_Y,  KEY_U,
-//     KEY_CAPS_LOCK,  KEY_A,      KEY_S,  KEY_D,  KEY_F,  KEY_G,  KEY_QUESTION,   KEY_VERTICAL_LINE,
-//     KEY_LSHIFT,     KEY_Z,      KEY_X,  KEY_C,  KEY_V,  KEY_B,  KEY_HOME,   KEY_END,
-//     KEY_LCTRL,      KEY_LGUI,   KEY_LALT,   KEY_E,  KEY_R,  KEY_T,  KEY_Y,  KEY_U,
-// };
+unsigned char keyMap[40] ={
+    KEY_ESC,        KEY_1,      KEY_2,  KEY_3,      KEY_4,  KEY_5,  KEY_THROW,  KEY_BACKSPACE,
+    KEY_TAB,        KEY_Q,      KEY_W,  KEY_E,      KEY_R,  KEY_T,  KEY_Y,  KEY_U,
+    KEY_CAPS_LOCK,  KEY_A,      KEY_S,  KEY_D,      KEY_F,  KEY_G,  KEY_H,  KEY_J,
+    KEY_LSHIFT,     KEY_HOME,   KEY_Z,  KEY_X,      KEY_C,  KEY_V,  KEY_B,  KEY_END,
+    KEY_F1,         KEY_F2,     KEY_F5, KEY_LSHIFT, KEY_LCTRL,  KEY_SPACE,    KEY_LALT,  KEY_ENTER,
+};
 
 // 这个就是键盘布局
-unsigned char keyMap[40] ={
-    KEY_LGUI,        KEY_1,      KEY_2,  KEY_3,  KEY_4,  KEY_5,  KEY_6,  KEY_7,
-    KEY_LALT,        KEY_Q,      KEY_W,  KEY_E,  KEY_R,  KEY_T,  KEY_Y,  KEY_U,
-    KEY_LCTRL,  KEY_A,      KEY_S,  KEY_D,  KEY_F,  KEY_G,  KEY_QUESTION,   KEY_VERTICAL_LINE,
-    KEY_LSHIFT,     KEY_Z,      KEY_X,  KEY_C,  KEY_V,  KEY_B,  KEY_HOME,   KEY_END,
-    KEY_LCTRL,      KEY_LGUI,   KEY_LALT,   KEY_E,  KEY_R,  KEY_T,  KEY_Y,  KEY_U,
-};
+// unsigned char keyMap[40] ={
+//     KEY_LGUI,        KEY_1,      KEY_2,  KEY_3,  KEY_4,  KEY_5,  KEY_6,  KEY_7,
+//     KEY_LALT,        KEY_Q,      KEY_W,  KEY_E,  KEY_R,  KEY_T,  KEY_Y,  KEY_U,
+//     KEY_LCTRL,  KEY_A,      KEY_S,  KEY_D,  KEY_F,  KEY_G,  KEY_QUESTION,   KEY_VERTICAL_LINE,
+//     KEY_LSHIFT,     KEY_Z,      KEY_X,  KEY_C,  KEY_V,  KEY_B,  KEY_HOME,   KEY_END,
+//     KEY_LCTRL,      KEY_LGUI,   KEY_LALT,   KEY_E,  KEY_SPACE,  KEY_T,  KEY_Y,  KEY_U,
+// };
