e1675e7a0fc1694e042c4aa15deb1b8f7e1fa890
black <rj45usb@163.com>
2020-04-07 00:22:28 +0800

    新加STM32的实现

---
+++ STM32F103C8T6_HID_keyboard/Debug/Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c 
+
+OBJS += \
+./Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.o 
+
+C_DEPS += \
+./Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.o: ../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c
+	arm-none-eabi-gcc -c "$<" -mcpu=cortex-m3 -std=gnu11 -g3 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -Og -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
